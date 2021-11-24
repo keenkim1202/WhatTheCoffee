6f57ac45b4903c8987a54802e50a75101baae572
KEEN <homurahomu@naver.com>
2021-11-24 21:26:41 +0900

    addRecordVC - 별점 버튼 구현

--- WhatTheCoffee/Sources/RecordsTab/AddRecordViewController.swift
+++ WhatTheCoffee/Sources/RecordsTab/AddRecordViewController.swift
@@ -16,8 +16,13 @@ class AddRecordViewController: UIViewController {
   @IBOutlet weak var recordImageView: UIImageView!
   @IBOutlet weak var addImageButton: UIButton!
   @IBOutlet weak var titleTextField: UITextField!
-  @IBOutlet weak var commandTextView: UITextView!
+  @IBOutlet weak var commentTextView: UITextView!
   
+  @IBOutlet weak var verygoodButton: UIButton!
+  @IBOutlet weak var goodButton: UIButton!
+  @IBOutlet weak var sosoButton: UIButton!
+  @IBOutlet weak var badButton: UIButton!
+  @IBOutlet weak var verybadButton: UIButton!
   
   // MARK: - View Life-Cycle
   override func viewDidLoad() {
@@ -29,6 +34,45 @@ class AddRecordViewController: UIViewController {
   // MARK: - Configure
   func configure() {
     self.addImageButton.tintColor = UIColor.imageButtonColor
+  }
+  
+  func updateRate(_ rate: Rate) {
+    switch rate {
+    case .veryGood:
+      verygoodButton.isSelected = true
+      goodButton.isSelected = false
+      sosoButton.isSelected = false
+      badButton.isSelected = false
+      verybadButton.isSelected = false
+      
+    case .good:
+      verygoodButton.isSelected = false
+      goodButton.isSelected = true
+      sosoButton.isSelected = false
+      badButton.isSelected = false
+      verybadButton.isSelected = false
+      
+    case .soso:
+      verygoodButton.isSelected = false
+      goodButton.isSelected = false
+      sosoButton.isSelected = true
+      badButton.isSelected = false
+      verybadButton.isSelected = false
+      
+    case .bad:
+      verygoodButton.isSelected = false
+      goodButton.isSelected = false
+      sosoButton.isSelected = false
+      badButton.isSelected = true
+      verybadButton.isSelected = false
+      
+    case .veryBad:
+      verygoodButton.isSelected = false
+      goodButton.isSelected = false
+      sosoButton.isSelected = false
+      badButton.isSelected = false
+      verybadButton.isSelected = true
+    }
   }
   
   // MARK: - Actions
@@ -47,5 +91,21 @@ class AddRecordViewController: UIViewController {
   @IBAction func onAddImage(_ sender: UIButton) {
     
   }
+  
+  @IBAction func onRateButton(_ sender: Any) {
+    guard let button = sender as? UIButton else { return }
+    guard
+      let text = button.titleLabel?.text,
+      let rate = Rate(rawValue: button.tag)
+    else { return }
+    
+    let tag = button.tag
+    print(text, tag)
+    
+    updateRate(rate)
+    
+    
+  }
+  
   
 }
