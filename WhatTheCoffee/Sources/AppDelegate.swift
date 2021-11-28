84abfbe559e34c0dc944af214f3d507ef56a6915
KEEN <homurahomu@naver.com>
2021-11-28 18:08:13 +0900

    화면 세로 고정

--- WhatTheCoffee/Sources/AppDelegate.swift
+++ WhatTheCoffee/Sources/AppDelegate.swift
@@ -12,8 +12,12 @@ import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+  
   var window: UIWindow?
+  
+  func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+    return UIInterfaceOrientationMask.portrait
+  }
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
