05c554d5acc5170e501b0f48e11a8697b29052d0
KEEN <homurahomu@naver.com>
2021-11-28 13:56:04 +0900

    addRecordVC - datepicker 추가

--- WhatTheCoffee/Sources/Extensions/DateFormatter++Extension.swift
+++ WhatTheCoffee/Sources/Extensions/DateFormatter++Extension.swift
@@ -9,8 +9,14 @@ import Foundation
 
 extension DateFormatter {
   static var visitDateFormat: DateFormatter {
-    let date = DateFormatter()
-    date.dateFormat = "yyyy/MM/dd"
-    return date
+    let df = DateFormatter()
+    df.dateFormat = "yyyy/MM/dd"
+    return df
+  }
+  
+  static var selectDateFormat: DateFormatter {
+    let df = DateFormatter()
+    df.dateFormat = "yyyy년 MM월 dd일"
+    return df
   }
 }
