b90116dc4de1373831f50c291713715a18bfd009
KEEN <homurahomu@naver.com>
2021-12-02 17:58:59 +0900

    nearCafeCell - selectionStyle 변경

---
+++ WhatTheCoffee/Sources/CafeListTab/SearchResultViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchNearCafeViewController.swift
+//  WhatTheCoffee
+//
+//  Created by KEEN on 2021/12/02.
+//
+
+import UIKit
+
+class SearchNearCafeViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+}
