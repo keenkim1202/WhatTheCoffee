8f9cea3463a6b9c38e85b6f68269d3209271a815
KEEN <homurahomu@naver.com>
2021-12-03 13:26:28 +0900

    locale 설정

--- WhatTheCoffee/Sources/Extensions/DateFormatter++Extension.swift
+++ WhatTheCoffee/Sources/Extensions/DateFormatter++Extension.swift
@@ -10,12 +10,16 @@ import Foundation
 extension DateFormatter {
   static var visitDateFormat: DateFormatter {
     let df = DateFormatter()
+    df.locale = Locale(identifier: "ko-KR")
+    df.timeZone = .autoupdatingCurrent
     df.dateFormat = "yyyy/MM/dd"
     return df
   }
   
   static var selectDateFormat: DateFormatter {
     let df = DateFormatter()
+    df.locale = Locale(identifier: "ko-KR")
+    df.timeZone = .autoupdatingCurrent
     df.dateFormat = "yyyy년 MM월 dd일"
     return df
   }
