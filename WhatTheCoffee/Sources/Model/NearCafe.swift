566aa241c0bae275ce9033a238ba0c88a2462180
KEEN <homurahomu@naver.com>
2021-12-09 14:47:41 +0900

    nearCafe를 realm 모델로 변경 및 적용

--- WhatTheCoffee/Sources/Model/NearCafe.swift
+++ WhatTheCoffee/Sources/Model/NearCafe.swift
@@ -6,10 +6,24 @@
 //
 
 import Foundation
+import RealmSwift
 
-struct NearCafe {
-  var name: String
-  var address: String
-  var point: (Double, Double)
-  var placeUrl: String
+// 커피 목록에 보일 커피 모델
+class NearCafe: Object {
+  @Persisted var name: String
+  @Persisted var address: String
+  @Persisted var latitude: Double
+  @Persisted var longitude: Double
+  @Persisted var placeUrl: String
+
+  @Persisted(primaryKey: true) var _id: ObjectId
+  
+  convenience init(name: String, address: String, latitude: Double, longitude: Double, placeUrl: String) {
+    self.init()
+    self.name = name
+    self.address = address
+    self.latitude = latitude
+    self.longitude = longitude
+    self.placeUrl = placeUrl
+  }
 }
