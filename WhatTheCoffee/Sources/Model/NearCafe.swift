82fffc83fa535fe9deada351577058795ea24213
KEEN <homurahomu@naver.com>
2021-12-09 14:53:36 +0900

    nearCafe - realm 안쓰는 방향으로 수정

--- WhatTheCoffee/Sources/Model/NearCafe.swift
+++ WhatTheCoffee/Sources/Model/NearCafe.swift
@@ -6,24 +6,11 @@
 //
 
 import Foundation
-import RealmSwift
 
-// 커피 목록에 보일 커피 모델
-class NearCafe: Object {
-  @Persisted var name: String
-  @Persisted var address: String
-  @Persisted var latitude: Double
-  @Persisted var longitude: Double
-  @Persisted var placeUrl: String
-
-  @Persisted(primaryKey: true) var _id: ObjectId
-  
-  convenience init(name: String, address: String, latitude: Double, longitude: Double, placeUrl: String) {
-    self.init()
-    self.name = name
-    self.address = address
-    self.latitude = latitude
-    self.longitude = longitude
-    self.placeUrl = placeUrl
-  }
+struct NearCafe {
+  var name: String
+  var address: String
+  var latitude: Double
+  var longitude: Double
+  var placeUrl: String
 }
--- WhatTheCoffee/Sources/Service/NearCafeRepository.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NearCafeRepository.swift.swift
-//  WhatTheCoffee
-//
-//  Created by KEEN on 2021/12/09.
-//
-
-import Foundation
