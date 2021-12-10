399e652b06b7c58ad41440ae61bcd921579479b8
KEEN <homurahomu@naver.com>
2021-12-10 16:27:22 +0900

    custom marker 추가

--- WhatTheCoffee/Sources/CafeListTab/CafeLocationViewController.swift
+++ WhatTheCoffee/Sources/CafeListTab/CafeLocationViewController.swift
@@ -14,18 +14,19 @@ class CafeLocationViewController: UIViewController {
   // MARK: - Properties
   var environment: Environment? = nil
   var nearCafeLists: [NearCafe] = []
+  var locationManger = CLLocationManager()
   var myLocation: CLLocationCoordinate2D?
   
   // MARK: - UI
-  @IBOutlet weak var mapView: NMFMapView!
+  @IBOutlet weak var naverMapView: NMFNaverMapView!
   
   // MARK: - View Life-Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    configureLocationManager()
+    
     if !nearCafeLists.isEmpty {
-      print(nearCafeLists.first!.latitude, nearCafeLists.first!.longitude)
-      mapView.positionMode = .direction
       moveCamera(lat: myLocation!.latitude, long: myLocation!.longitude)
       
       for cafe in nearCafeLists {
@@ -36,8 +37,26 @@ class CafeLocationViewController: UIViewController {
     } else {
       print("cafeList is empty.")
     }
-//    moveCamera(lat: 37.5666102, long: 126.9783881)
-//    pinMaker(lat: 37.5670135, long: 126.9783740)
+  }
+  
+  // MARK: - Configure
+  func configureLocationManager() {
+    locationManger.delegate = self
+    locationManger.desiredAccuracy = kCLLocationAccuracyBest
+    locationManger.requestWhenInUseAuthorization()
+    
+    naverMapView.showLocationButton = true
+    
+    if CLLocationManager.locationServicesEnabled() {
+        print("위치 서비스 On 상태")
+     locationManger.startUpdatingLocation()
+      
+      if let location = locationManger.location {
+        myLocation = location.coordinate
+      }
+    } else {
+        print("위치 서비스 Off 상태")
+    }
   }
   
   // MARK: 지도 시점 이동
@@ -46,7 +65,8 @@ class CafeLocationViewController: UIViewController {
     cameraUpdate.reason = 3
     cameraUpdate.animation = .fly
     cameraUpdate.animationDuration = 2
-    mapView.moveCamera(cameraUpdate, completion: { (isCancelled) in
+    
+    naverMapView.mapView.moveCamera(cameraUpdate, completion: { (isCancelled) in
       if isCancelled {
         print("카메라 이동 취소")
       } else {
@@ -62,7 +82,11 @@ class CafeLocationViewController: UIViewController {
       print("마커 클릭됨")
       return true
     }
-    marker.mapView = mapView
+    
+    marker.iconImage = NMFOverlayImage(name: "coffee_marker_shadow")
+    marker.width = 30
+    marker.height = 33
+    marker.mapView = naverMapView.mapView
   }
   
   // MARK: - Action
@@ -70,4 +94,22 @@ class CafeLocationViewController: UIViewController {
     self.dismiss(animated: true, completion: nil)
   }
   
+}
+
+// MARK: - CLLocationManagerDelegate -
+extension CafeLocationViewController: CLLocationManagerDelegate {
+  // 위치 정보 계속 업데이트 -> 위도 경도 받아옴
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    print("didUpdateLocations")
+    if let location = locations.first {
+      myLocation = location.coordinate
+      print("위도: \(location.coordinate.latitude)")
+      print("경도: \(location.coordinate.longitude)")
+    }
+  }
+  
+  // 위도 경도 받아오기 에러
+  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    print(error)
+  }
 }
