27720f08208275317562577dc5c60f6de0e52a39
KEEN <homurahomus@gmail.com>
2026-08-12 17:27:08 +0900

    사진 라이브러리를 PHPicker로 전환

--- WhatTheCoffee/Sources/Presentation/Recommend/AddCoffeeViewController.swift
+++ WhatTheCoffee/Sources/Presentation/Recommend/AddCoffeeViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import PhotosUI
 import FirebaseAnalytics
 import TextFieldEffects
 
@@ -151,15 +152,22 @@ class AddCoffeeViewController: BaseViewController {
   }
 
   // MARK: - Photo Library & Camera Access
+  /// 사진 라이브러리는 PHPicker를 쓴다. 앱 밖에서 뜨기 때문에 UIImagePickerController보다 빠르고
+  /// 사진 접근 권한도 필요 없다. 카메라는 PHPicker가 다루지 못해 기존 방식을 유지한다.
   private func openLibrary() {
-    imagePicker.sourceType = .photoLibrary
-    present(imagePicker, animated: false)
+    var configuration = PHPickerConfiguration()
+    configuration.filter = .images
+    configuration.selectionLimit = 1
+
+    let picker = PHPickerViewController(configuration: configuration)
+    picker.delegate = self
+    present(picker, animated: true)
   }
 
   private func openCamera() {
     if UIImagePickerController.isSourceTypeAvailable(.camera) {
       imagePicker.sourceType = .camera
-      present(imagePicker, animated: false)
+      present(imagePicker, animated: true)
     } else {
       showErrorAlert("카메라 사용이 불가합니다.\n권한을 확인해주세요.")
     }
@@ -268,5 +276,30 @@ extension AddCoffeeViewController: UITextFieldDelegate {
   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
     nameTextField.resignFirstResponder()
     return true
+  }
+}
+
+// MARK: - PHPickerViewControllerDelegate
+extension AddCoffeeViewController: PHPickerViewControllerDelegate {
+  func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+    picker.dismiss(animated: true)
+
+    let provider = results.first?.itemProvider
+    guard let provider, provider.canLoadObject(ofClass: UIImage.self) else { return }
+
+    // iCloud에 있는 사진은 내려받느라 시간이 걸린다. 그 사이 저장하면 이전 이미지가 저장된다.
+    setAnalyzing(true)
+
+    provider.loadObject(ofClass: UIImage.self) { object, _ in
+      DispatchQueue.main.async { [weak self] in
+        guard let self else { return }
+        guard let image = object as? UIImage else {
+          setAnalyzing(false)
+          showErrorAlert("사진을 불러오지 못했습니다.")
+          return
+        }
+        applyCutout(to: image)
+      }
+    }
   }
 }
