a8bad8864bf450e7eaa09bab2867a8fba24d1ac5
KEEN <homurahomus@gmail.com>
2026-08-13 14:25:04 +0900

    네비게이션 바 색상과 폰트를 앱 톤에 맞추고 유리 배경 제거
    iOS 26이 바 버튼 뒤에 까는 유리 재질은 appearance로 제어되지 않아
    항목마다 hidesSharedBackground를 지정한다.

--- WhatTheCoffee/Sources/App/BaseViewController.swift
+++ WhatTheCoffee/Sources/App/BaseViewController.swift
@@ -8,5 +8,12 @@ class BaseViewController: UIViewController {
     
     adjustNavigationBarFont()
   }
-  
+
+  /// 바 버튼은 viewDidLoad 뒤에 만들어지는 화면도 있어 표시 직전에 한 번 더 훑는다.
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    NavigationBarAppearance.hideSharedBackground(on: navigationItem.leftBarButtonItems)
+    NavigationBarAppearance.hideSharedBackground(on: navigationItem.rightBarButtonItems)
+  }
 }
